64efcc9b9201d745f7d567650644b5904553c6cb
Cameron Palmer <palmtrey779@gmail.com>
2021-06-17 11:29:55 -0400

    Working on updater.py

--- HAWK/updater.py
+++ HAWK/updater.py
@@ -2,6 +2,10 @@
 # the update process for HAWK
 
 import gdown
+import os
+
 url = 'https://drive.google.com/uc?id=1u7nVqfgCpCB7z1wv38vDpWk8XVz28eT2'
 output = 'data/temp/me-addresses.txt'
-gdown.download(url, output)
+if not os.path.isfile(output):
+    os.mkdir('data/temp')
+    gdown.download(url, output)
