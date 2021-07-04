f757ff744ce4f81b6ed1eeaf37fbc2c972b31f94
Cameron Palmer <palmtrey779@gmail.com>
2021-07-04 17:01:11 -0400

    Working on book extraction

--- HAWK/src/book_extractor.py
+++ HAWK/src/book_extractor.py
@@ -2,6 +2,6 @@ import pdfminer.layout as pl
 import pdfminer.high_level as hl
 
 format = pl.LAParams(char_margin = 10, all_texts = True)
-text = hl.extract_text('book.pdf', format)
-file = open('out1.txt', 'w+')
+text = hl.extract_text('../data/tax_books/wayne_book.pdf', page_numbers = [2])
+file = open('out.txt', 'w+')
 file.write(text)
