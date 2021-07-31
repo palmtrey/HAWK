3d07b4200eff2aaf7deda74758425798d8f2e510
Cameron Palmer <palmtrey779@gmail.com>
2021-07-31 08:02:51 -0400

    Last commit - recoding in C#

--- HAWK/src/cross_referencer.py
+++ HAWK/src/cross_referencer.py
@@ -7,30 +7,39 @@ def cross_reference(tax_dir, waterfront_dir, zip):
 
     town_addresses = []
     csvout = 'Billing Address, Local Address, Zipcode, Longitude, Latitude\n'
+    i = 1
     for waterfront_address in tqdm(waterfront_file):
+        #print(i)
+        #if i != 32:
+            #i += 1
+            #continue
+
         tax_file = open(tax_dir)
         if re.search(zip, waterfront_address):
             temp = ' '.join(waterfront_address.split(',')[0:2])
             temp = re.sub(' Shore Rd', ' Shore', temp)
+            temp = re.sub(' N Gayton Ln', ' North Gayton Lane', temp)
             temp = re.sub('N Campers Point Rd', 'North Campers Point', temp)
             temp = re.sub('Maranacook Station Ln', 'Maranacook Station', temp)
             temp = re.sub(' Rd', ' Road', temp)
             temp = re.sub(' Dr', ' Drive', temp)
             temp = re.sub(' Ln', ' Lane', temp)
             temp = re.sub(' St', ' Street', temp)
+            temp = re.sub(' Trl', ' Trail', temp)
             temp = re.sub('\s', '', temp)
             temp = temp.upper()
+            #print(temp)
             for address in tax_file:
 
                 if re.search('^' + temp + '$', re.sub('\s', '', address.split(',')[1].upper())):
                     csvout += address.split(',')[0] + ',' + waterfront_address.split(',')[0] + ' ' + waterfront_address.split(',')[1] + ',' + waterfront_address.split(',')[3] + ',' + waterfront_address.split(',')[4] + ',' + re.sub('\n', '', waterfront_address.split(',')[5]) + '\n'
-
+        #i += 1
     tax_file.close()
     waterfront_file.close()
-    with open('final-readfield-maranacook.csv', 'w+') as file:
+    with open('../data/partial-finals/lower-narrows-final-100m.csv', 'w+') as file:
         file.write(csvout)
             #print(temp)
 
 
 if __name__ == '__main__':
-    cross_reference('../data/tax_books/tax_book_csvs/readfield.csv', '../data/waterfront-address-csvs/maranacook_lake.csv', '04355')
+    cross_reference('../data/tax_books/tax_book_csvs/winthrop.csv', '../data/waterfront-address-csvs/lower_narrows_pond.csv', '04364')
--- HAWK/src/main.py
+++ HAWK/src/main.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import os
 import plotter
 import startup.updater as updater
-
+import startup.lake_extractor as extractor
 
 # HAWK entry point
 if __name__ == '__main__':
@@ -11,15 +11,20 @@ if __name__ == '__main__':
     # Update the data in the project using updater.py
     #updater.update()
 
-    # Find waterfront properties in updated data
-    #plotter.findWaterfront('maranacook_lake', ['04364', '04355'], distance_from = 100)
+    #extractor.extractPolygons('../data/lake/apple_valley_lake')
 
-    plotter.findWaterfront('cobbossee_lake', ['04350', '04351', '04259', '04265', '04345', '04364'], distance_from = 100)
+
+    # Find waterfront properties in updated data
+    plotter.findWaterfront('lower_narrows_pond', ['04364'], distance_from = 100)
+
+    #plotter.findWaterfront('annabessacook_lake', ['04259', '04265', '04364'], distance_from = 100)
 
     # Plot waterfront properties
+    '''
     plt.figure()
     ax = plt.axes(projection=ccrs.Orthographic())
     plotter.plotMaranacookLake(ax, 'blue', 'maranacook_lake')
     plotter.plotMaranacookIslands(ax, 'green', 'maranacook_lake')
     plotter.plotAddresses(ax, 'red', 'maranacook_lake')
     plt.show()
+    '''
